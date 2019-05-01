ca877ceadc9558496eaf4496e6f2ab45fa25663a
Milad Bourhani <milad.bourhani@gmail.com>
2019-05-01 21:15:54 +0200

    stuff

--- src/MainAsync.hs
+++ src/MainAsync.hs
@@ -6,27 +6,6 @@ import Pipes.Concurrent
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
 
-user :: Effect IO Event
-user = lift userInput
-
-tick :: Producer Event IO r
-tick = forever $ do
-    lift $ threadDelay 1000000
-    yield T
-
-handler :: World -> Consumer Event IO ()
-handler = loop
-  where
-    loop w = do
-      lift $ putStrLn $ show w
-      worldEvents <- lift $ runTimeouts w
-      userEvent <- await
-      let w' = foldl think w (userEvent : worldEvents)
-      if alive w' then
-        loop w'
-      else
-        return ()
-
 game :: World -> IO ()
 game w = do
     (output, input) <- spawn unbounded
@@ -37,5 +16,23 @@ game w = do
     forkIO $ do runEffect $ tick >-> toOutput output
                 performGC
     
-    runEffect $ fromInput input >-> (handler w)
+    runEffect $ fromInput input >-> (loop w)
 
+loop :: World -> Consumer Event IO ()
+loop w = do
+  lift $ putStrLn $ show w
+  worldEvents <- lift $ runTimeouts w
+  userEvent <- await
+  let w' = foldl think w (userEvent : worldEvents)
+  if alive w' then
+    loop w'
+  else
+    return ()
+
+user :: Effect IO Event
+user = lift userInput
+
+tick :: Producer Event IO r
+tick = forever $ do
+    lift $ threadDelay 1000000
+    yield T
