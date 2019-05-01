26ad38427df4bda2d6313b8689dcaf400a805cc2
Milad Bourhani <milad.bourhani@gmail.com>
2019-05-01 21:05:48 +0200

    stuff

--- app/Main.hs
+++ app/Main.hs
@@ -8,12 +8,12 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  let w = World 0 0 True 0 []
   if (length args) /= 1
     then
       putStrLn "Wrong syntax"
     else do
       putStrLn "Welcome!"
+      let w = World 0 0 True 0 []
       if (head args) == "A"
         then A.game w
         else T.game w
